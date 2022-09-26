f5784990c70d73b33873a08572fa118e83a7c621
Soroushiii <soroushradmard@gmail.com>
2022-09-26 13:18:11 +0330

    remove schema / minify docs

--- assets/scripts/popup-script.js
+++ assets/scripts/popup-script.js
@@ -1,14 +1,1 @@
- document.getElementById("open-popup").addEventListener("click", function(){
-     document.getElementsByClassName("popup")[0].classList.add("active");
- })
-
- document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
-     document.getElementsByClassName("popup")[0].classList.remove("active");
- })
-
-function toggle(){
-    var blur = document.getElementById('overlay');
-    blur.classList.toggle('active')
-    var popup = document.getElementById('popup-section');
-    popup.classList.toggle('active')
-}
+function toggle(){document.getElementById("overlay").classList.toggle("active"),document.getElementById("popup-section").classList.toggle("active")}document.getElementById("open-popup").addEventListener("click",function(){document.getElementsByClassName("popup")[0].classList.add("active")}),document.getElementById("dismiss-popup-btn").addEventListener("click",function(){document.getElementsByClassName("popup")[0].classList.remove("active")});
